d6083d7732cc7e90558523b652c80e2b65b2d951
Kia Farhang <kia.farhang@nytimes.com>
2023-10-10 16:13:32 -0500

    feature: update endpoint to fetch a single product

--- lesson1/lesson1_original.go
+++ lesson1/lesson1_original.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -8,6 +10,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 )
 
 func main() {
@@ -15,8 +18,9 @@ func main() {
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		request, err := http.NewRequestWithContext(r.Context(), "GET", "https://dummyjson.com/products", nil)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+	r.Get("/products/{productID}", func(w http.ResponseWriter, r *http.Request) {
+		request, err := http.NewRequestWithContext(r.Context(), "GET", fmt.Sprintf("https://dummyjson.com/products/%s", chi.URLParam(r, "productID")), nil)
 		if err != nil {
 			http.Error(w, http.StatusText(500), 500)
 			return
@@ -46,7 +50,19 @@ func main() {
 			return
 		}
 
-		w.Write(bodyAsBytes)
+		var product Product
+
+		err = json.Unmarshal(bodyAsBytes, &product)
+
+		if err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+
+		if err := render.Render(w, r, &product); err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
 
 	})
 	err := http.ListenAndServe(":4000", r)
@@ -54,4 +70,14 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to start server, error was %v", err)
 	}
+}
+
+type Product struct {
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+func (p *Product) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
