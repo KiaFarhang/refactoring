3931afdd02a501941ba1e949d283772b2c553349
Kia Farhang <kia.farhang@nytimes.com>
2023-10-10 14:57:58 -0500

    feature: implement basic call to /products dummy JSON endpoint

--- lesson1/lesson1_original.go
+++ lesson1/lesson1_original.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -10,9 +12,42 @@ import (
 
 func main() {
 	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hi!"))
+		request, err := http.NewRequestWithContext(r.Context(), "GET", "https://dummyjson.com/products", nil)
+		if err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+
+		httpClient := &http.Client{Timeout: time.Second * 5}
+		response, err := httpClient.Do(request)
+
+		if err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+
+		defer response.Body.Close()
+
+		statusCode := response.StatusCode
+
+		if statusCode >= 400 {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+
+		bodyAsBytes, err := io.ReadAll(response.Body)
+
+		if err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+
+		w.Write(bodyAsBytes)
+
 	})
 	err := http.ListenAndServe(":4000", r)
 
